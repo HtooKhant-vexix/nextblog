5cafb4dd7a063159ed3ac1c48811b71c732f451f
HtooKhant-vexix <vexixongaming02@gmail.com>
2024-05-31 11:10:51 +0700

    g

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,32 @@ import Image from "next/image";
 import { db } from "./lib/db";
 
 export default async function Home() {
-  async function getPosts() {
-    const response = await db.post.findMany({
-      select: {
-        id: true,
-        title: true,
-        content: true,
-        tag: true,
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-    return response;
-  }
+  // async function getPosts() {
+  //   const response = await db.post.findMany({
+  //     select: {
+  //       id: true,
+  //       title: true,
+  //       content: true,
+  //       tag: true,
+  //     },
+  //     orderBy: {
+  //       createdAt: "desc",
+  //     },
+  //   });
+  //   return response;
+  // }
 
-  const posts = await getPosts();
+  // const posts = await getPosts();
 
-  console.log(posts);
+  // console.log(posts);
 
   return (
     <main className="grid items-center justify-center md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
       {/* <main className="flex min-h-screen flex-col items-center justify-between p-24"> */}
-      {posts.map((post) => (
+      {/* {posts.map((post) => (
         <PostCard key={post.id} post={post} />
-      ))}
+      ))} */}
+      <div className="">hello</div>
     </main>
   );
 }
